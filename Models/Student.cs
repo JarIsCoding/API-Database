dced444ce160e16686e23fd96ea97c0ccf17e8de
JarIsCoding <derpyderp1098@gmail.com>
2024-01-12 11:17:10 -0800

    commit api

---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+namespace API_Database.Models;
+public class Student
+{
+    public int Id { get; set; }
+    
+    public string? Name { get; set; }
+    public string? Last { get; set; }
+    public string? Hobby { get; set; }
+}
